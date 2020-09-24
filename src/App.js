60d8003c11e6082e346b00c34e6c17ad50b4d6b8
Greys73 <71707245+Greys73@users.noreply.github.com>
2020-09-24 19:24:58 +0400

    get weather API
    get weather API

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = "4dac21171a72bb216190c6db9bb2ddb2";
+const API_CITY = "479123";
+
+const data = {
+	city : '73',
+	temp : undefined,
+	wind : undefined	
+}
+
+const  getWeather = async () => {
+	const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=${API_CITY}&appid=${API_KEY}`);
+	const api_data =  await api_url.json();
+	data.city = api_data.name;
+	data.temp = api_data.main.temp;
+	data.wind = api_data.wind.speed;
+	console.log(api_data);
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">	
+		<div>
+		  <button onClick={getWeather}>Get json</button>
+		</div>		
     </div>
   );
 }
